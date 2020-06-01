0c4c1a2c0467c07bdb855d9c0220d38efacec79a
Donato Freda <donato.barossi@outlook.com>
2020-06-01 12:29:22 +0200

    2

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ class App:
                 try:
                     self.data  = utLogParse(line)
                     print(self.data)
-                    self.switch.get(self.data['TYPE'], None)
+                    #self.switch.get(self.data['TYPE'], None)
                 except Exception as e:
                     logging.error("An error occurred while elaborating this line: [%s]" % line)
                     logging.error(e)
