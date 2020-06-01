0b4df748e5609dffb3f2dd3deaedf7f098df2a0b
Donato Freda <donato.barossi@outlook.com>
2020-06-01 12:44:57 +0200

    fix newlines array

--- util_file_reader.py
+++ util_file_reader.py
@@ -5,16 +5,15 @@ class FileReader :
     def __init__(self, path = cfg.UrtPath + "/" + cfg.UrtLogPath):
         self.path = path
         self.readsize = 0
-        self.newlines = []
 
     def getNewLines(self):
+        newlines = []
         if os.path.getsize(self.path) > self.readsize:
             _file = open(self.path, "r")
             _file.seek(self.readsize)
             newlines = _file.read().split("\n")
             _file.close()
             self.readsize = os.path.getsize(self.path)
-            return newlines
         elif os.path.getsize(self.path) < self.readsize:
             self.readsize = os.path.getsize(self.path)
-            return None
+        return newlines
