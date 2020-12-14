8b3f2d5f55afaeaf8b45ff89b8beef42a5ff6d89
Donato Freda <donato.barossi@outlook.com>
2020-12-14 21:09:14 +0100

    add new permissions

--- ut_cmd.py
+++ ut_cmd.py
@@ -3,6 +3,7 @@ import random
 
 UtCmds = {
     'gametype' : "g_gametype %s; reload",
+    'reload' : "reload",
     'map' : "map %s",
     'next' : "g_nextmap %s",
     'cycle' : "cyclemap",
@@ -25,13 +26,13 @@ AppCmds = {
 CustomComds = ['allow', 'deny', 'protection', 'kill', 'maplist', 'ban' ]
 
 Auth = {
-    'BF75A818B7749C51A991EB4EF4CB71DB' : ['gtype', 'map', 'next', 'cycle', 'private', 'only', 'all', 'allow', 'deny', 'kick', 'slap', 'smite', 'nuke', 'timelimit', 'julianka', 'alexa', 'maplist'], # Dr.Geo
-    '44721144924F5A46C8C91CC53C725A76' : ['gtype', 'map', 'next', 'cycle', 'private', 'only', 'all', 'allow', 'deny', 'kick', 'slap', 'smite', 'nuke', 'timelimit', 'julianka', 'alexa', 'maplist'], # g1n8
-    'C9F3DCA4B00F1EA9FA6A17E00642AC52' : ['gtype', 'map', 'next', 'cycle', 'private', 'only', 'all', 'allow', 'deny', 'kick', 'slap', 'smite', 'nuke', 'timelimit', 'julianka', 'alexa', 'maplist'], # GJterror
-    'A17B770E3619E2E1420DD21648ACD7E5' : ['gtype', 'map', 'next', 'cycle', 'private', 'only', 'all', 'allow', 'deny', 'kick', 'slap', 'smite', 'nuke', 'timelimit', 'julianka', 'alexa', 'maplist' ], # Capopattuglia
-    'ABA582DE5E260E4C9C73D49CD66CBDDE' : ['gtype', 'map', 'next', 'cycle', 'private', 'only', 'all', 'allow', 'deny', 'kick', 'slap', 'smite', 'nuke', 'timelimit', 'julianka', 'alexa', 'maplist'], # REVENGE
-    '7012BEC27E05320119AF799B962C3A97' : ['gtype', 'map', 'next', 'cycle', 'private', 'only', 'all', 'allow', 'deny', 'kick', 'slap', 'smite', 'nuke', 'timelimit', 'julianka', 'alexa', 'maplist'], # Maverick
-    'D94814531BDCFD66595CC834F6EC5F87' : ['gtype', 'map', 'next', 'cycle', 'private', 'only', 'all', 'allow', 'deny', 'kick', 'slap', 'smite', 'nuke', 'timelimit', 'julianka', 'alexa', 'maplist'], # giogio79
+    'BF75A818B7749C51A991EB4EF4CB71DB' : ['gtype', 'map', 'next', 'cycle', 'private', 'only', 'all', 'allow', 'deny', 'kick', 'slap', 'smite', 'nuke', 'timelimit', 'julianka', 'alexa', 'maverick', 'porky', 'reload', 'maplist'], # Dr.Geo
+    '44721144924F5A46C8C91CC53C725A76' : ['gtype', 'map', 'next', 'cycle', 'private', 'only', 'all', 'allow', 'deny', 'kick', 'slap', 'smite', 'nuke', 'timelimit', 'julianka', 'alexa', 'maplist', 'maverick', 'porky', 'reload'], # g1n8
+    'C9F3DCA4B00F1EA9FA6A17E00642AC52' : ['gtype', 'map', 'next', 'cycle', 'private', 'only', 'all', 'allow', 'deny', 'kick', 'slap', 'smite', 'nuke', 'timelimit', 'julianka', 'alexa', 'maplist', 'maverick', 'porky'], # GJterror
+    'A17B770E3619E2E1420DD21648ACD7E5' : ['gtype', 'map', 'next', 'cycle', 'private', 'only', 'all', 'allow', 'deny', 'kick', 'slap', 'smite', 'nuke', 'timelimit', 'julianka', 'alexa', 'maplist', 'maverick', 'porky' ], # Capopattuglia
+    'ABA582DE5E260E4C9C73D49CD66CBDDE' : ['gtype', 'map', 'next', 'cycle', 'private', 'only', 'all', 'allow', 'deny', 'kick', 'slap', 'smite', 'nuke', 'timelimit', 'julianka', 'alexa', 'maplist', 'maverick', 'porky'], # REVENGE
+    '7012BEC27E05320119AF799B962C3A97' : ['gtype', 'map', 'next', 'cycle', 'private', 'only', 'all', 'allow', 'deny', 'kick', 'slap', 'smite', 'nuke', 'timelimit', 'julianka', 'alexa', 'maplist', 'maverick', 'porky'], # Maverick
+    'D94814531BDCFD66595CC834F6EC5F87' : ['gtype', 'map', 'next', 'cycle', 'private', 'only', 'all', 'allow', 'deny', 'kick', 'slap', 'smite', 'nuke', 'timelimit', 'julianka', 'alexa', 'maplist', 'maverick', 'porky'], # giogio79
     '6F80784BD6672C739E8DFE010F3D063B' : ['*']  # dr.barossi
 }
 
